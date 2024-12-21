fccf3f275b62a671fb2a96f7c4b08fb3f414b9be
SlemanMH <slemanhmidat6@gmail.com>
2024-12-21 23:37:34 +0200

    react

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import Portfolio from './components/portfolio/Portfolio'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import { Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <StartBootsraps />
-      <Portfolio />
-      <About />
-      <Contact />
+
+      <Routes>
+        <Route path="/" element={<StartBootsraps />} ></Route>
+        <Route path="/Portfolio" element={<Portfolio />} ></Route>
+        <Route path="/About" element={<About />} ></Route>
+        <Route path="/Contact" element={<Contact />} ></Route>
+      </Routes>
+      
       <Footer />
     </>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-   
+    <header className='header position-sticky top-0 left-0'>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container">
-          <a className="navbar-brand fs-2" href="#">START BOOTSTRAP</a>
+          <Link className="navbar-brand fs-2" to={'/'}>START BOOTSTRAP</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ms-auto">
-              <a className="nav-link active" aria-current="page" href="#">PORTFOLIO</a>
-              <a className="nav-link" href="#">ABOUT</a>
-              <a className="nav-link" href="#">CONTACT</a>
+              <Link className="nav-link active" aria-current="page" to={'/Portfolio'}>PORTFOLIO</Link>
+              <Link className="nav-link" to={'/About'}>ABOUT</Link>
+              <Link className="nav-link" to={'/Contact'}>CONTACT</Link>
             </div>
           </div>
         </div>
       </nav>
-  
+    </header>
   )
 }
